12d66163789b1458ae1703cd4e3c22e208d7f13c
min-0 <dhe08133@naver.com>
2021-03-09 17:23:45 +0900

    Turtle Graphic

---
+++ 정민영_과제1.py
@@ -0,0 +1,30 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.shape('turtle')
+
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+t.forward(100)
+t.right(144)
+
+t.penup()
+t.backward(200)
+t.pendown()
+
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
