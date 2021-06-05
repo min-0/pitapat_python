dbd1fd63e75caeb34a2db6968f4fcbcc91e12ed4
min-0 <dhe08133@naver.com>
2021-06-06 00:57:31 +0900

    7장 이름공간

--- 7장 실습문제.py
+++ 7장 실습문제.py
@@ -22,9 +22,37 @@
 #print('x = {}, f(0) = {}'.format(x, res))
 
 # 7.3
-def safe_opne(filename, mode):
-    try:
-        infile = open(filename, mode)
-        return infile
-    except:
-        return None
+# def safe_opne(filename, mode):
+#    try:
+#        infile = open(filename, mode)
+#        return infile
+#    except:
+#        return None
+
+# 7.4
+
+# def h(n):
+#     print('Start h')
+#     try:
+#         print(1/n)  # 오류가 발생하는 문장
+#     except:
+#         print('Caught!')
+#     print(n)
+
+# def g(n):
+#     try:
+#         print('Start g')
+#         try:
+#             h(n-1)
+#         except:
+#             print('Caught!')
+#         print(n)
+#     except:
+#         print('Caught!')
+
+# def f(n):
+#     print('Start f')
+#     g(n-1)
+#     print(n)
+
+# f(2)
